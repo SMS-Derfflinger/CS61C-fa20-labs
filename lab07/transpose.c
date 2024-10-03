abeef0d4fe41ba9fe8ab6953741f5319bfcaeaf0
Heinz <aurelianob808@gmail.com>
2024-10-03 18:49:34 +0800

    finish lab07 exercise3

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -12,5 +12,13 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
-    // YOUR CODE HERE
+    for (int i = 0; i < n; i += blocksize) {
+        for (int j = 0; j < n; j += blocksize) {
+            for (int k = i; k < i + blocksize && k < n; k++) {
+                for (int l = j; l < j + blocksize && l < n; l++) {
+                    dst[k + l * n] = src[l + k * n];
+                }
+            }
+        }
+    }
 }
